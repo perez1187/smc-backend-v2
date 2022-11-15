86933b2316b33c0c3afe5979d0b664b87c27c2f9
perez1187 <o.perez1187@gmail.com>
2022-11-15 15:39:14 +0100

    add static root

--- api/settings.py
+++ api/settings.py
@@ -109,6 +109,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 COUNTRIES_FLAG_URL = 'flags/{code}.png'
 
 # Default primary key field type
